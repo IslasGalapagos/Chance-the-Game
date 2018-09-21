53a0848302679f6fbcba62d81b22362c93888f9e
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-09-21 18:10:06 +0300

    test: configure ts-jest

--- jest/setup.js
+++ jest/setup.js
@@ -1,7 +1,4 @@
-const React = require('react');
 const {configure} = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-
-global.React = React;
 
 configure({adapter: new Adapter()});
