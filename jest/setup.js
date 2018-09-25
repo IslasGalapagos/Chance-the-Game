84c8db88456185e514be77c38045b88ccd09f5c1
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-09-25 20:03:57 +0300

    chore: update jest (babel, emotion serializer, jsdom features)

--- jest/setup.js
+++ jest/setup.js
@@ -1,4 +1,27 @@
-const {configure} = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import jsdom from 'jsdom';
 
 configure({adapter: new Adapter()});
+
+const {JSDOM} = jsdom;
+const dom = new JSDOM(
+  '<!DOCTYPE html><html><body><div id="root"></div></body></html>'
+);
+
+window.eval(`
+  Object.defineProperty(window.document.body, 'clientHeight', {
+    get() {
+      return window.innerHeight;
+    }
+  });
+`);
+
+global.window = window;
+global.document = window.document;
+
+global.window.resizeTo = (width, height) => {
+  global.window.innerWidth = width || global.window.innerWidth;
+  global.window.innerHeight = width || global.window.innerHeight;
+  global.window.dispatchEvent(new Event('resize'));
+};
