4d2c6f21c20b7ae93ed8490f1957ecf912817d62
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-09-26 16:28:24 +0300

    test: update App
    add Count

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -2,10 +2,16 @@ import * as React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
 import Bg from './Bg';
+import Count from './Count';
 
 describe('App', () => {
   it('contains <Bg/>', () => {
     const component = shallow(<App />);
     expect(component.contains(<Bg />)).toBeTruthy();
+  });
+
+  it('contains <Count/>', () => {
+    const component = shallow(<App />);
+    expect(component.contains(<Count score={[0, 0]} />)).toBeTruthy();
   });
 });
