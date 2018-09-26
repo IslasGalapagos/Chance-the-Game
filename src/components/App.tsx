c4c9b934e8b38bd8c9bb04d3d93604144265afa7
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-09-26 16:28:58 +0300

    feat: update App
    add Count

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Bg from './Bg';
+import Count from './Count';
 
 class App extends React.PureComponent<{}, {}> {
   render() {
     return (
       <React.Fragment>
         <Bg />
+        <Count score={[0, 0]} />
       </React.Fragment>
     );
   }
