8f28761d89ecb1e9d6a9658fc333cd3b328f6921
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-10-01 00:45:17 +0300

    test: update App
    add StartingScreen

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
+import StartingScreen from './StartingScreen';
 import Bg from './Bg';
 import Count from './Count';
 
 describe('App', () => {
+  it('contains <StartingScreen/>', () => {
+    const component = shallow(<App />);
+    expect(component.contains(<StartingScreen />)).toBeTruthy();
+  });
+
   it('contains <Bg/>', () => {
     const component = shallow(<App />);
     expect(component.contains(<Bg />)).toBeTruthy();
