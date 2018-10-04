71027bd2800963ba9c30201f0964844fcc095500
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-10-05 00:29:09 +0300

    chore: add license header to some files

--- src/components/LetsStartBlock.spec.tsx
+++ src/components/LetsStartBlock.spec.tsx
@@ -1,3 +1,11 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Copyright © 2018 Evgeny Sysoletin. All rights reserved.
+*/
+
 import {shallow} from 'enzyme';
 import * as React from 'react';
 import LetsStartBlock from './LetsStartBlock';
