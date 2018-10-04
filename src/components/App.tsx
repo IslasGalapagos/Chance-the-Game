717e11e94d45f93a4442c30a9b34daf432737949
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-10-05 00:50:07 +0300

    feat: update App
    add Game

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Copyright © 2018 Evgeny Sysoletin. All rights reserved.
+*/
+
 import * as React from 'react';
 import StartingScreen from './StartingScreen';
 import Bg from './Bg';
 import Count from './Count';
+import Game from './Game';
 
 class App extends React.PureComponent<{}, {}> {
   render() {
@@ -9,8 +18,9 @@ class App extends React.PureComponent<{}, {}> {
       <React.StrictMode>
         <React.Fragment>
           <StartingScreen />
-          <Bg />
           <Count score={[0, 0]} />
+          <Game />
+          <Bg />
         </React.Fragment>
       </React.StrictMode>
     );
