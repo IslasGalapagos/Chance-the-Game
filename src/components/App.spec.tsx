bcd81ff781d6aef3609b6d09155710a77ff94c49
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-10-05 00:48:54 +0300

    test: update App
    add Game

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -1,9 +1,18 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Copyright © 2018 Evgeny Sysoletin. All rights reserved.
+*/
+
 import * as React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
 import StartingScreen from './StartingScreen';
 import Bg from './Bg';
 import Count from './Count';
+import Game from './Game';
 
 describe('App', () => {
   it('contains <StartingScreen/>', () => {
@@ -19,5 +28,10 @@ describe('App', () => {
   it('contains <Count/>', () => {
     const component = shallow(<App />);
     expect(component.contains(<Count score={[0, 0]} />)).toBeTruthy();
+  });
+
+  it('contains <Game/>', () => {
+    const component = shallow(<App />);
+    expect(component.contains(<Game />)).toBeTruthy();
   });
 });
