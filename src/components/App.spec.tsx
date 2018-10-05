29513be506f0ede17ff8a9d81b9769d5342541a9
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-10-05 20:21:08 +0300

    test: update App
    add TotalScore

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -12,6 +12,7 @@ import App from './App';
 import StartingScreen from './StartingScreen';
 import Bg from './Bg';
 import Score from './Score';
+import TotalScore from './TotalScore';
 import Game from './Game';
 import ScreenChecker from './ScreenChecker';
 
@@ -39,5 +40,14 @@ describe('App', () => {
   it('contains <ScreenChecker/>', () => {
     const component = shallow(<App />);
     expect(component.contains(<ScreenChecker />)).toBeTruthy();
+  });
+
+  it('contains <TotalScore/>', () => {
+    const component = shallow(<App />);
+    expect(
+      component.contains(
+        <TotalScore name="Diophantus" score={[150, 50]} coefficients={[5, 1]} />
+      )
+    ).toBeTruthy();
   });
 });
