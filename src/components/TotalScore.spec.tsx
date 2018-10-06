b24716677b90f4af9ae5671307bdc5c49e770666
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-10-07 01:16:07 +0300

    refactor: add -s to coefficient

--- src/components/TotalScore.spec.tsx
+++ src/components/TotalScore.spec.tsx
@@ -19,7 +19,9 @@ const props: Props = {
 
 describe('<TotalScore/>', () => {
   it('renders correctly', () => {
-    const treeAndStyles = testRenderer.create(<TotalScore  {...props} />).toJSON();
+    const treeAndStyles = testRenderer
+      .create(<TotalScore {...props} />)
+      .toJSON();
     expect(treeAndStyles).toMatchSnapshot();
   });
 
@@ -27,31 +29,60 @@ describe('<TotalScore/>', () => {
   it('sets props.name to username', () => {
     const component = shallow(<TotalScore {...props} />);
     const rows = component.find('li');
-    const userName = rows.at(0).children().at(1).text().split(': ')[0];
+    const userName = rows
+      .at(0)
+      .children()
+      .at(1)
+      .text()
+      .split(': ')[0];
     expect(userName).toEqual(props.name);
   });
 
   it('shows props.score for user and random', () => {
     const component = shallow(<TotalScore {...props} />);
     const rows = component.find('li');
-    const userScore = parseInt(rows.at(0).text().split(': ')[1], 10);
-    const randomScore = parseInt(rows.at(1).text().split(': ')[1], 10);
+    const userScore = parseInt(
+      rows
+        .at(0)
+        .text()
+        .split(': ')[1],
+      10
+    );
+    const randomScore = parseInt(
+      rows
+        .at(1)
+        .text()
+        .split(': ')[1],
+      10
+    );
     expect(userScore).toEqual(props.score[0]);
     expect(randomScore).toEqual(props.score[1]);
   });
 
-  it('shows coefficient for user and random if it greater than 1', () => {
+  it('shows coefficients for user and random if it greater than 1', () => {
     const component = shallow(<TotalScore {...props} />);
-    let userCoefficient = component.find('li').at(0).find('span');
-    let randomCoefficient = component.find('li').at(1).find('span');
+    let userCoefficient = component
+      .find('li')
+      .at(0)
+      .find('span');
+    let randomCoefficient = component
+      .find('li')
+      .at(1)
+      .find('span');
     expect(userCoefficient.length).toEqual(1);
     expect(randomCoefficient.length).toEqual(0);
     const userCoefficientNum = parseInt(userCoefficient.text(), 10);
     expect(userCoefficientNum).toEqual(props.coefficients[0]);
 
     component.setProps({coefficients: [1, 3]});
-    userCoefficient = component.find('li').at(0).find('span');
-    randomCoefficient = component.find('li').at(1).find('span');
+    userCoefficient = component
+      .find('li')
+      .at(0)
+      .find('span');
+    randomCoefficient = component
+      .find('li')
+      .at(1)
+      .find('span');
     expect(userCoefficient.length).toEqual(0);
     expect(randomCoefficient.length).toEqual(1);
     const randomCoefficientNum = parseInt(randomCoefficient.text(), 10);
